17690c3634acd80ec36c9ae9cded627d0ae37702
ShinDodam <tlseheka@naver.com>
2021-11-15 19:43:29 +0900

    first commit

---
+++ students/urls.py
@@ -0,0 +1,14 @@
+import view as view
+from . import views
+from django.urls import path
+
+app_name = 'students'
+urlpatterns = [
+    path('reg/', views.regStudent, name='reg'),
+    path('regCon/', views.regConStudent, name='regCon'),
+    path('all/', views.reaStudentAll, name='stuAll'),
+    path('<str:name>/det/', views.detStudent, name='stuDet'),
+    path('<str:name>/mod/', views.reaStudentOne, name='stuMod'),
+    path('modCon/', views.modConStudent, name='mod'),
+     path('<str:name>/del/', views.delConStudent, name='stuDel'),
+]
